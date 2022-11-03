02869e045853301b7dd9a8d20a68475d615a2d06
SebastianHinestroza12 <menasebastian1356@americana.edu.co>
2022-11-02 19:04:14 -0500

    fotos agregadas

--- README.md
+++ README.md
@@ -7,5 +7,3 @@ Para instalar todas las dependecias necesarias para correr el proyecto.
 ### `npm run dev`
 
 Ingresar en su navegador http://127.0.0.1:5173/
-
-### Link deploy en vercel
--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,7 +1,7 @@
 import "./Gallery.css";
-// import Gallery1 from "../../assets/gallery1.jfif";
+import Gallery1 from "../../assets/gallery1.jfif";
 import Gallery2 from "../../assets/gallery2.jpg";
-// import Gallery3 from "../../assets/gallery3.jfif";
+import Gallery3 from "../../assets/gallery3.jfif";
 import Gallery4 from "../../assets/pufi-nap2.png";
 import Gallery5 from "../../assets/gallery5.png";
 import Gallery6 from "../../assets/gallery6.jpg";
@@ -15,9 +15,9 @@ const Gallery = () => {
       </div>
 
       <div className="gallery-image">
-        <img src={Gallery2} alt="." />
+        <img src={Gallery1} alt="." />
         <img src={Gallery2} alt="" />
-        <img src={Gallery2} alt="." />
+        <img src={Gallery3} alt="." />
         <img src={Gallery4} alt="" />
         <img src={Gallery5} alt="" />
         <img src={Gallery6} alt="" />
